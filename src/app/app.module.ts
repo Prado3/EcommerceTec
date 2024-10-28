f426dad3f10e6c4b596de779b3f15dc1f714adfe
FedeMarianii <federicoomarianii@gmail.com>
2024-10-28 19:24:50 -0300

    avance carrito

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router'; // Asegúrate de importar esto
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { CartComponent } from './cart/cart.component'; // Asegúrate de que esta ruta sea correcta
+//import { ProductListComponent } from './products/product-list/product-list.component';
+import { CartService } from './cart/cart.service';
+//import { ProductsService } from './products/products.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+   // ProductListComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    RouterModule // Agrega esto aquí
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  //providers: [CartService, ProductsService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
