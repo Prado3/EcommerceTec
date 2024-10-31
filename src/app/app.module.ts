049e7515a7311fb54ef554f5c0ec9a43f2d550d2
Luciano Delmonte <lucianodelmonte00@gmail.com>
2024-10-31 20:35:33 -0300

    primera version del auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
-import { NgModule } from '@angular/core';
+// app.module.ts
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule, provideHttpClient } from '@angular/common/http'; // Importa HttpClientModule
+
 import { AppComponent } from './app.component';
+import { AuthService } from './auth/auth.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // otros componentes
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule // Agrega HttpClientModule aquí
   ],
-  providers: [],
+  providers: [provideHttpClient()],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/auth/register/register.component.ts
@@ -0,0 +1,33 @@
+import { Component} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+})
+export class RegisterComponent {
+  registerForm!: FormGroup;
+  registrationError: string | null = null;
+
+  constructor(private fb: FormBuilder, private authService: AuthService) {
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      name: ['', Validators.required],
+      lastName: ['', Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.registerForm.valid) {
+      this.authService.register(this.registerForm.value).subscribe({
+        next: () => alert('Usuario registrado con éxito'),
+        error: err => {
+          this.registrationError = err.message;
+          console.error('Error al registrarse:', err);
+        }
+      });
+    }
+  }
+}
