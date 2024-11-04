6ea21ea4a0d2e5c709ddf5c1000da78afb77c4a0
Franco Introcaso <introcasofranco@hotmail.com>
2024-11-03 23:17:55 -0300

    Agrega cambios de estilos generales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,16 @@
 // src/app/app-routing.module.ts
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProductListComponent } from './products/product-list/product-list.component';
-import { ProductDetailComponent } from './products/product-detail/product-detail.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: '', redirectTo: 'products', pathMatch: 'full' }, // Redirige la ruta principal a 'products'
-  { path: 'products', component: ProductListComponent },
-  { path: 'products/:id', component: ProductDetailComponent }, // Ruta para el detalle de producto
-  { path: '**', redirectTo: 'products' } // Ruta fallback
+  { path: '', component: HomeComponent }, // Ruta principal
+  { path: '**', redirectTo: '' }, // Redirección para rutas no encontradas
 ];
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
+
